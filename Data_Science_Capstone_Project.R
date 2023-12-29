d5e89e8545bf2843a9185b7411136c5f995d9567
sangwoo <138969571+rejeton1@users.noreply.github.com>
2023-12-29 21:05:41 +0900

    Add files via upload

--- Data_Science_Capstone_Project.R
+++ Data_Science_Capstone_Project.R
@@ -1,52 +1,66 @@
-data <- readLines("./final/en_US/en_US.twitter.txt", 6)
+library(LaF)
+library(stringr)
 
-data
+set.seed(123)
+
+#For sampling random lines, use function 'sample_lines' in LaF package.
+data <- sample_lines("./final/en_US/en_US.twitter.txt", n=10)
+
+#removing '\r' end of each lines.
+data <- str_remove(data, pattern="\\r$")
 
 
-#We will use 'str_split()' in stringr package
-#as main function for split charater.
+#We will use 'str_split()' in stringr package as main function for split charater.
 
 #can see about regular expressions by typing 
 #vignette("regular-expressions")
 
-#The turn of split
-#First. split by blank ("\\s")
+#The steps of split
+#First. split by blank (regex for any whitespace "\s")
+#about this expression, 's' is not an specia; ex. so it didn't need escaper.
+#so if we use "\s" to the command, it makes an error.
+#However for using regex "\s", we need an literal "\" not escaper "\"
+#so to use literal "\" we need additional "\" in front of "\".
+#conclusionally, "\\s" is correct.
 
 data <- str_split(data, "\\s")
+#Then black is removed, and remaining things are splitted.
+
 
 #For futher split, we need to look around regular expression entirely.
 
 #Second, split the punctuation
 #If i use str_split based on punctuation, that punctuations
 #are removed. so i decided to write new function to split punctuation.
+#-> i don't need to make a new function! i can do the same thing with existing functions
 
-lapply
-grep("[[:punct:]]", data[[1]])
-str_locate_all(data[[1]][3], "[[:punct:]]")
-str_sub(data[[1]][3], 4,4)
-append(벡터,삽입할데이터,after=n)
+#to keep the delimitter, we can use the expression "(?=anything)".
+#about this expression
+#lookaround assertion
+#there are 4 kinds : positive/negative, lookahead/lookbehind
+#These assertions don't “consume” any characters.
+#it means, it use "".
+#positive look-ahead assertion. Matches if ... matches at the current input.
+#so, in "a$", $ is following the a. and because assertion doesn't consume any characters,
+#it is divided to "a", "$" not "a", ""(not consuming "$")
+#also, in "$c", $ is following the "". so it divided into "", "$c" not "", "c"
+#after this, you can guess the effect of the other 3 assertions.
+#additionally, when matching, start from "a", but end of it, end to "" after the "$", not "$" itself.
+#lookbehind go from right to left.
 
-#With this basic functions, let's make a new function!
 
-for(i in length(data)){
-  index <- grep("[[:punct:]]", data[[i]])
-  for(t in index){
-    if(length(data[[i]][t])==1){
-      next()
-    }
-    locate <- str_locate(data[[i]][t], "[[:punct:]]")
-    if(locate[[1]][1] == 1){
-      word1 <- str_sub(data[[i]][t], locate[[1]][1], locate[[1]][2])
-      word2 <- str
-    }else if(locate[[1]][1] > 1 && locate[[1]][2] < length(data[[i]][t])){
-      
-    }else if(locate[[1]][2] == length(data[[i]][t]))
-    
-    # punct <- str_sub(data[[1]][3], locate[[1]][1], locate[[1]][2])
-    # append(data[[i]])
-    
-  }
-}
+  data <- lapply(data, str_split, pattern="(?=[:punct:])")
+  data <- lapply(data, unlist)
+  data <- lapply(data, str_split, pattern="(?<=[:punct:])")
+  data <- lapply(data, unlist)
+  data <- lapply(data, str_split, pattern="(?=[$])")
+  data <- lapply(data, unlist)
+  data <- lapply(data, str_split, pattern="(?<=[$])")
+  data <- lapply(data, unlist)
+  data <- lapply(data, function(x) return(x[x !=""]))
 
+#After doning this, we split the punctuation.
+  
+data
 
 

d2f8e7f55e395980321bf5cfae1ba213a58da661
sangwoo <138969571+rejeton1@users.noreply.github.com>
2024-02-20 19:58:33 +0900

    Add files via upload

--- addMI.R
+++ addMI.R
@@ -10,16 +10,6 @@ for(package in packages){
 }
 
 
-n_line <- 10
-data1 <- readLines("./final/en_US/en_US.blogs.txt", n=n_line)
-data2 <- readLines("./final/en_US/en_US.news.txt", n=n_line)
-data3 <- readLines("./final/en_US/en_US.twitter.txt", n=n_line)
-
-data <- c(data1, data2, data3)
-
-#Make a corpus
-corpus <- VCorpus(VectorSource(data))
-
 ################################################################################
 #Data preprocessing
 
@@ -52,14 +42,13 @@ preprocess_corpus <- function(corpus){
   corpus <- tm_map(corpus, removeNumbers)
   corpus <- tm_map(corpus, stripWhitespace)
   corpus <- tm_map(corpus, removeWords, profanity)
+  corpus <- tm_map(corpus, removeWords, "$")
   corpus <- tm_map(corpus, stripWhitespace)
   corpus <- tm_map(corpus, content_transformer(function(x) str_remove(x, pattern="\\s$")))
   corpus <- tm_map(corpus, content_transformer(function(x) str_remove(x, pattern="^\\s")))
   return(corpus)
 }
 
-
-corpus <- preprocess_corpus(corpus)
 
 
 split_into_sentence <- function(preprocessed_text){
@@ -69,9 +58,6 @@ split_into_sentence <- function(preprocessed_text){
   text <- str_trim(text)
 }
 
-corpus <- tm_map(corpus, content_transformer(split_into_sentence))
-
-textdata <- data.table(text=unlist(sapply(corpus, as.character)))
 
 #input : sentence, output : wordpairs
 extract_wordpair <- function(sentence, n=3){
@@ -82,7 +68,11 @@ extract_wordpair <- function(sentence, n=3){
   if(len >= 4){
     for(i in 1:(len-n)){
       for(j in (i+n):(len)){
-        pairs[length(pairs)+1] <- paste(tokens[i], tokens[j])
+        if(tokens[i] == tokens[j]){
+          next
+        } else {
+          pairs[[length(pairs)+1]] <- paste(tokens[i], tokens[j])
+        }
       }
     }
     return(pairs)
@@ -90,8 +80,50 @@ extract_wordpair <- function(sentence, n=3){
 }
 
 
+n_line <- 70000
+data1 <- readLines("./final/en_US/en_US.blogs.txt", n=n_line)
+data2 <- readLines("./final/en_US/en_US.news.txt", n=n_line)
+data3 <- readLines("./final/en_US/en_US.twitter.txt", n=n_line)
+
+data <- c(data1, data2, data3)
+
+#Make a corpus
+corpus <- VCorpus(VectorSource(data))
+
+corpus <- preprocess_corpus(corpus)
+
+corpus <- tm_map(corpus, content_transformer(split_into_sentence))
+
+textdata <- data.table(text=unlist(sapply(corpus, as.character)))
+
+wordpairs <- apply(textdata, 1, extract_wordpair)
 
 
+wordpairtable <- data.table(table(unlist(wordpairs)))[order(-N)]
+# wordpairtable <- wordpairtable[N > 10]
+wordpairtable <- wordpairtable[,.(x=str_split_i(V1, "\\s", 1), y=str_split_i(V1, "\\s", 2), N), by=V1]
+colnames(wordpairtable) <- c('pair', 'x', 'y', 'c')
+all_c <- sum(wordpairtable$c)
 
+wordpairtable_cx <- wordpairtable[,.(cx=sum(c)), by=x][order(-cx)]
+colnames(wordpairtable_cx) <- c("X", "cx")
+wordpairtable_cy <- wordpairtable[,.(cy=sum(c)), by=y][order(-cy)]
+colnames(wordpairtable_cy) <- c("Y", "cy")
+
+find_cx <- function(x){
+  return(wordpairtable_cx[X==x, cx])
+}
+
+find_cy <- function(y){
+  return(wordpairtable_cy[Y==y, cy])
+}
+
+wordpairtable <- wordpairtable[,.(x, y, c, 
+                                  PMI=log((c*all_c)/(find_cx(x)*find_cy(y)))), by=pair]
+
+###############################################################################
+#filtering important wordpair(c > 10, PMI >= 0)
+
+wordpairtable_important <- wordpairtable[c > 10 & PMI >= 0]
 
 

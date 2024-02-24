98b45bfdd2469840cb536973562b372ba4fc8714
sangwoo <138969571+rejeton1@users.noreply.github.com>
2024-02-24 19:37:51 +0900

    Add files via upload

--- LaunchMarkov.R
+++ LaunchMarkov.R
@@ -15,6 +15,4 @@ for(package in packages){
   library(package, character.only = TRUE)
 }
 
-
-
-load("markov.RData")
+load(url("https://github.com/rejeton1/Data_Science_Capstone_Project/raw/main/markov.RData"))
